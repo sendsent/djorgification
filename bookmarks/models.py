1c2b732fe68ab54f2af6f5dc4c6f3dc0e0323670
KickMe <kickme@alifs-mbp.gateway.sonic.net>
2018-05-11 11:20:20 -0700

    Add time created/updated fields to Bookmark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -8,4 +8,7 @@ class Bookmark (models.Model):
     url = models.URLField('URL', unique=True) # unique=true can be translated to a constraint in the SQL that will actually restrict URLs to be unique in the DB
     name = models.CharField(max_length=300)
     notes = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True) # every time a new entity is added this will automatically be set to "now", which should be the time it was added at
+    last_modified = models.DateTimeField(auto_now=True) # update the time stamp whenever the entity is edited 
+    # page_pic = models.ImageField(upload_to='pic_folder', default='pic_folder/None/no-img.jpg')
 
