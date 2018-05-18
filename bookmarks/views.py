6c5fbeeb4f30753993b794a334637546409f9b3b
KickMe <kickme@alifs-mbp.gateway.sonic.net>
2018-05-17 17:50:38 -0700

    Expose personal bookmarks in templacte/view for logged in users

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,11 +1,16 @@
 # views will essentially be that middle layer getting data from the ORM (the models) and returning it to the template so we can actually look at it
 
 from django.shortcuts import render
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
 # index view that takes a request
 def index(request):
     context = {} # he mentioned python dictionary? put stuff in here soon. analogous to what we've done with express and react
     # TODO: business logic, get data, etc. - do some stuff to the context
     context['bookmarks'] = Bookmark.objects.all()
+    if request.user.is_anonymous:
+        context['personal_bookmarks'] = PersonalBookmark.objects.none()
+    else:
+        context['personal_bookmarks'] = PersonalBookmark.objects.filter(
+            user=request.user)
     return render(request, 'bookmarks/index.html', context)
